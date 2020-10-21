2730ab62893df9d1a3e3356282801d7e1c68e13d
Anders Geil <anders.geil@gmail.com>
2020-10-21 18:46:14 -0400

    Added helper to stream and process .xz files line by line

---
+++ StreamXZ.py
@@ -0,0 +1,37 @@
+import json
+import lzma
+import sys
+
+file = str(sys.argv[1])
+
+with lzma.open(file, mode='rt') as fh:
+    target_file = file.split('.xz')[0] + '.json'
+
+    for i, line in enumerate(fh):
+        object = json.loads(line)
+
+        # do something with the object here
+        vars = ['id', 'created_utc', 'subreddit_id', 'title', 'url',
+                'author', 'subreddit', 'domain', 'score', 'num_comments']
+        try:
+            post = dict()
+            for var in vars:
+                try:
+                    post[var] = object[var]
+                except:
+                    pass
+
+            if 'id' in post.keys():
+                with open(target_file, 'a') as tf:
+                    json.dump(post, tf)
+                    tf.write('\n')
+            else:
+                print('Missing ID:')
+                print(file)
+                print("Line {}: {}".format(i, line))
+                print("Skipping...\n")
+        except:
+            print('Error in:')
+            print(file)
+            print("Line {}: {}".format(i, line))
+            print("Skipping...\n")
